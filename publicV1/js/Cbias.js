c9ca2399f8200ffd2888ac2cc5efda28fe13d4f7
vivek <vivek2020bheke@gmail.com>
2022-11-19 11:35:42 +0530

    all the newsletter pages

---
+++ publicV1/js/Cbias.js
@@ -0,0 +1,8 @@
+
+
+function affiliateLinkClicked() {
+
+    const analytics = firebase.analytics();
+    analytics.logEvent('C bias Affiliate link Clicked');
+    //console.log("affiliate link clicked");
+} 
---
+++ publicV1/js/strategy.js
@@ -0,0 +1,8 @@
+
+
+function affiliateLinkClicked() {
+
+    const analytics = firebase.analytics();
+    analytics.logEvent('Marketing Newsletter Affiliate link Clicked');
+    //console.log("affiliate link clicked");
+} 
